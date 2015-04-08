f966f4b474ed0c30110b1e3867b910fa6540ec89
zaprice <zprice11@gmail.com>
2015-04-08 19:28:34 -0400

    implemented key schedule

--- impl/des.rb
+++ impl/des.rb
@@ -1,10 +1,12 @@
 class DES
 
-  attr_accessor :key, :tmp_subkey
+  attr_accessor :key, :left_subkey, :right_subkey, :ctr
+
+  shifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
   def encrypt block
     block = initial_permutation block
-    16.times do
+    for @ctr in 0..15
       left = block[0..3]
       right = block[4..7]
       block = append(right, xor(left, feistel(right)))
@@ -14,20 +16,20 @@ class DES
 
   def initial_permutation block
     permutation = File.read("../config/initial_permutation.txt").strip.split " "
-    return permute block, permutation
+    permute block, permutation
   end
 
   def final_permutation block
     permutation = File.read("../config/final_permutation.txt").strip.split " "
-    return permute block, permutation
+    permute block, permutation
   end
 
-  def feistel half-block
-    bit_shuffle( s_box( xor( expand(half-block), get_subkey)))
+  def feistel  half_block
+    bit_shuffle( s_box( expand( half_block).zip(get_subkey).map { |byte, key| xor(byte, key) }))
   end
 
   # Aids diffusion of key material
-  def expand
+  def expand  half_block
 
   end
 
@@ -39,23 +41,38 @@ class DES
 
   # Diffusion of key material and s-box changes
   # Each bit of input should change many bits of output
-  def bit_shuffle half-block
+  def bit_shuffle  half_block
 
   end
 
   # Returns round keys for each Feistel node
   # Relies on internal state of tmp_subkey
   def get_subkey
+    if ctr == 0
+      selected_bits = bytes_to_bits permuted_choice_1 @key
+      @left_subkey = selected_bits[0..27]
+      @right_subkey = selected_bits[28..55]
+    end
 
+    @left_subkey = shift @left_subkey, shifts[ctr]
+    @right_subkey = shift @right_subky, shift[ctr]
+
+    permuted_choice_2 @left_subkey.add(@right_subkey)
   end
 
-  def permuted_choice_1
-
+  def permuted_choice_1 key
+    permutation = File.read("../config/permuted_choice_1.txt").strip.split " "
+    permute key, permutation
   end
 
-  def permuted_choice_2
-
+  def permuted_choice_2 key
+    permutation = File.read("../config/permuted_choice_2.txt").strip.split " "
+    permute key, permutation
   end
+
+  ##############################################################################
+  # Helper functions
+  ##############################################################################
 
   def xor(hex1, hex2)
     [hex1].pack("H*").bytes.zip([hex2].pack("H*").bytes).map { |arr| arr[0] ^ arr[1] }.pack("c*").unpack("H*").first
@@ -72,7 +89,12 @@ class DES
   def permute block, permutation
     bits = bytes_to_bits(block)
     permuted_bits = permutation.map { |index| bits[index - 1]}
-    return bits_to_bytes permuted_bits
+    bits_to_bytes permuted_bits
+  end
+
+  def shift subkey, value
+    removed = subkey.shift value
+    subkey.push(removed).flatten
   end
 
 end
