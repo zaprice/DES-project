72d9bc465c683d9825fac489bc10c5066044fd56
zaprice <zprice11@gmail.com>
2015-04-08 18:56:12 -0400

    more work, added initial/final permutations

--- impl/des.rb
+++ impl/des.rb
@@ -5,24 +5,25 @@ class DES
   def encrypt block
     block = initial_permutation block
     16.times do
-      left = block[0..7]
-      right = block[8..15]
+      left = block[0..3]
+      right = block[4..7]
       block = append(right, xor(left, feistel(right)))
     end
-    return final_permutation block
+    final_permutation block
   end
 
-  def initial_permutation
-
+  def initial_permutation block
+    permutation = File.read("../config/initial_permutation.txt").strip.split " "
+    return permute block, permutation
   end
 
-  def final_permutation
-
+  def final_permutation block
+    permutation = File.read("../config/final_permutation.txt").strip.split " "
+    return permute block, permutation
   end
 
   def feistel half-block
-    expanded_block = expand half-block
-    return bit_shuffle(s_box(xor(expanded_block, get_subkey)))
+    bit_shuffle( s_box( xor( expand(half-block), get_subkey)))
   end
 
   # Aids diffusion of key material
@@ -32,18 +33,46 @@ class DES
 
   # Ensures non-linearity from plaintext to ciphertext
   # Each bit of ciphertext should depend on many bits of input
-  def s_box
+  def s_box expanded_block
 
   end
 
   # Diffusion of key material and s-box changes
   # Each bit of input should change many bits of output
-  def bit_shuffle
+  def bit_shuffle half-block
 
   end
 
+  # Returns round keys for each Feistel node
+  # Relies on internal state of tmp_subkey
   def get_subkey
 
   end
-  
+
+  def permuted_choice_1
+
+  end
+
+  def permuted_choice_2
+
+  end
+
+  def xor(hex1, hex2)
+    [hex1].pack("H*").bytes.zip([hex2].pack("H*").bytes).map { |arr| arr[0] ^ arr[1] }.pack("c*").unpack("H*").first
+  end
+
+  def bytes_to_bits block
+    block.map { |byte| hex_to_bin byte }.flatten
+  end
+
+  def hex_to_bin hex
+    [hex].pack("H*").bytes[0].to_s(2).split("").map { |bit| bit.to_i }
+  end
+
+  def permute block, permutation
+    bits = bytes_to_bits(block)
+    permuted_bits = permutation.map { |index| bits[index - 1]}
+    return bits_to_bytes permuted_bits
+  end
+
 end
